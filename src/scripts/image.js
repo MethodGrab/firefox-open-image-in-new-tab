ef3e3b07184016624bf6b708c5fc59084e9e4d5a
Nick <methodgr4b@gmail.com>
2019-06-16 18:13:44 +0100

    :recycle: Refactoring

--- src/scripts/image.js
+++ src/scripts/image.js
@@ -1,17 +1,19 @@
 ;(function(  ) { // eslint-disable-line no-extra-semi
+
 	const imageClass = 'rendered-image';
-	const scaledClass = 'is-scaled';
+	const isScaledClass = 'is-scaled';
 	const canScaleClass = 'can-scale';
+	const imageSelector = `.${imageClass}`;
 
 	const renderImage = src => {
 		const img = document.createElement( 'img' );
 		img.src = src;
-		img.classList.add( imageClass, scaledClass );
+		img.classList.add( imageClass, isScaledClass );
 		document.body.prepend( img );
 	};
 
 	const canScale = _ => {
-		const img = document.querySelector(`.${imageClass}`);
+		const img = document.querySelector( imageSelector );
 		const w = document.documentElement.clientWidth < img.naturalWidth;
 		const h = document.documentElement.clientHeight < img.naturalHeight;
 
@@ -19,13 +21,17 @@
 	};
 
 	const setCanScale = _ => {
-		const img = document.querySelector(`.${imageClass}`);
+		const img = document.querySelector( imageSelector );
 
 		if ( canScale() ) {
-			img.classList.add(canScaleClass);
+			img.classList.add( canScaleClass );
 		} else {
-			img.classList.remove(canScaleClass);
+			img.classList.remove( canScaleClass );
 		}
+	};
+
+	const toggleScaled = _ => {
+		document.querySelector( imageSelector ).classList.toggle( isScaledClass );
 	};
 
 	const data = window.location.hash.substr( 1 );
@@ -33,17 +39,16 @@
 	if ( data.length > 0 ) {
 		renderImage( data );
 		setCanScale();
+
+		window.addEventListener( 'resize', e => {
+			setCanScale();
+		});
+
+		document.body.addEventListener( 'click', e => {
+			if ( e.target.matches( imageSelector ) && canScale() ) {
+				toggleScaled();
+			}
+		});
 	}
 
-	window.addEventListener( 'resize', e => {
-		setCanScale();
-	});
-
-	document.body.addEventListener( 'click', e => {
-		const img = `.${imageClass}`;
-
-		if ( e.target.matches( img ) && canScale() ) {
-			document.querySelector( img ).classList.toggle( scaledClass );
-		}
-	});
 }(  ));
